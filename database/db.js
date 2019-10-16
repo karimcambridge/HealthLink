d392286fc5af0c92ed5f6e1c9ffa71fbbbf3435c
Kar <karimcambridge@gmail.com>
2019-10-16 16:04:54 -0400

    Eslint auto fix various files `/eslint --fix`

--- database/db.js
+++ database/db.js
@@ -3,17 +3,17 @@ const Sequelize = require('sequelize');
 
 const db = {};
 const sequelize = new Sequelize('healthlink', 'healthlink', 'x5P05C5Wtglz', {
-    host: 'localhost',
-    dialect: 'mariadb',
-    dialectOptions: { connectTimeout: 1000 }, // mariadb connector option
-    operatorsAliases: false,
+  host: 'localhost',
+  dialect: 'mariadb',
+  dialectOptions: { connectTimeout: 1000 }, // mariadb connector option
+  operatorsAliases: false,
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
 
 db.sequelize = sequelize;
--- models/User.js
+++ models/User.js
@@ -2,31 +2,31 @@ const Sequelize = require('sequelize');
 const db = require('../database/db.js');
 
 module.exports = db.sequelize.define(
-    'accounts',
-    {
-        id: {
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        first_name: {
-            type: Sequelize.STRING
-        },
-        last_name: {
-            type: Sequelize.STRING
-        },
-        email: {
-            type: Sequelize.STRING
-        },
-        password: {
-            type: Sequelize.STRING
-        },
-        created: {
-            type: Sequelize.DATE,
-            defaultValue: Sequelize.NOW
-        }
+  'accounts',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        timestamps: false
-    }
+    first_name: {
+      type: Sequelize.STRING,
+    },
+    last_name: {
+      type: Sequelize.STRING,
+    },
+    email: {
+      type: Sequelize.STRING,
+    },
+    password: {
+      type: Sequelize.STRING,
+    },
+    created: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW,
+    },
+  },
+  {
+    timestamps: false,
+  },
 );
