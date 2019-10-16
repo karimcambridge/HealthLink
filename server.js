16ca466b3ccefe1cab1b4b2e40accdc9179540a1
Kar <karimcambridge@gmail.com>
2019-10-16 15:31:30 -0400

    server.js file

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+
+const Users = require("./routes/Users");
+
+app.use('/users', Users);
+
+app.listen(port, () => {
+    console.log("Server is running on port: " + port);
+});
+
