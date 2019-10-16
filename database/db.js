6e5d56b955764b45b557f51b05bc7b79a1e42ab5
Kar <karimcambridge@gmail.com>
2019-10-16 15:31:13 -0400

    database setup
    Using Sequelize and mariadb.

---
+++ database/db.js
@@ -0,0 +1,22 @@
+
+const Sequelize = require('sequelize');
+
+const db = {};
+const sequelize = new Sequelize('healthlink', 'user', 'pw', {
+    host: 'localhost',
+    dialect: 'mariadb',
+    dialectOptions: { connectTimeout: 1000 }, // mariadb connector option
+    operatorsAliases: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
